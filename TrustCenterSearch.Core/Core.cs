1249755d4c863f5f63366c404b68d91b6206e87c
Kai.Bratvogel <kai.bratvogel@haevg-rz.de>
2020-07-01 14:20:40 +0200

    #23

--- TrustCenterSearch.Core/Core.cs
+++ TrustCenterSearch.Core/Core.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using TrustCenterSearch.Core.DataManagement;
 using TrustCenterSearch.Core.Models;
