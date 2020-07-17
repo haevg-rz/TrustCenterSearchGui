3aec10fd729c78320fbfba42e180a92f733e6cd7
Kai.Bratvogel <kai.bratvogel@haevg-rz.de>
2020-07-17 14:41:42 +0200

    #146

--- TrustCenterSearch.Core/DataManagement/TrustCenters/TrustCenterManager.cs
+++ TrustCenterSearch.Core/DataManagement/TrustCenters/TrustCenterManager.cs
@@ -10,21 +10,13 @@ namespace TrustCenterSearch.Core.DataManagement.TrustCenters
     internal class TrustCenterManager
     {
         #region Properties
-        internal ImportManager ImportManager { get; set; }
-        internal DownloadManager DownloadManager { get; set; }
+        internal ImportManager ImportManager { get; set; } = new ImportManager();
+        internal DownloadManager DownloadManager { get; set; } = new DownloadManager();
 
         #endregion
 
         #region Fields
         private readonly string _dataFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\TrustCenterSearch\data\";
-        #endregion
-
-        #region Constructor
-        internal TrustCenterManager()
-        {
-            this.DownloadManager = new DownloadManager();
-            this.ImportManager = new ImportManager();
-        }
         #endregion
 
         #region InternalMethods
