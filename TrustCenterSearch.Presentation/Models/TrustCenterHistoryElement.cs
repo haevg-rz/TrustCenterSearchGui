fd75e4581697eedf7fb68a03f2389a664de95d98
Kai.Bratvogel <kai.bratvogel@haevg-rz.de>
2020-07-27 16:16:07 +0200

    #192

---
+++ TrustCenterSearch.Presentation/Models/TrustCenterHistoryElement.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+using TrustCenterSearch.Core.Models;
+
+namespace TrustCenterSearch.Presentation.Models
+{
+    public class TrustCenterHistoryElement : ViewModelBase
+    {
+        public TrustCenterMetaInfo TrustCenterMetaInfo { get; set; }
+
+        private bool _active = true;
+
+        public bool Active
+        {
+            get => this._active;
+            set => Set(() => this.Active, ref this._active, value);
+        }
+
+        public TrustCenterHistoryElement(TrustCenterMetaInfo trustCenterMetaInfo)
+        {
+            TrustCenterMetaInfo = trustCenterMetaInfo;
+        }
+    }
+}
