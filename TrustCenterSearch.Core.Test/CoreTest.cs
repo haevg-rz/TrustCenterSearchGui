2134075fff87788794507748e7afdc4c0c1d1e75
tobias.meissner <tobias.meissner@haevg-rz.de>
2020-07-27 12:30:02 +0200

    #188

---
+++ TrustCenterSearch.Core.Test/CoreTest.cs
@@ -0,0 +1,84 @@
+﻿
+using Xunit;
+
+namespace TrustCenterSearchCore.Test
+{
+    public class CoreTest
+    {
+        [Fact(DisplayName = "ImportAllCertificatesFromTrustCentersAsyncTest")]
+        public void ImportAllCertificatesFromTrustCentersAsyncTest()
+        {
+            #region Arrange
+
+            #endregion
+
+
+            #region Act
+
+            #endregion
+
+
+            #region Assert
+
+            #endregion
+
+        }
+
+        [Fact(DisplayName = "AddTrustCenterAsyncTest")]
+        public void AddTrustCenterAsyncTest()
+        {
+            #region Arrange
+
+            #endregion
+
+
+            #region Act
+
+            #endregion
+
+
+            #region Assert
+
+            #endregion
+
+        }
+        
+        [Fact(DisplayName = "ReloadCertificatesOfTrustCenterTest")]
+        public void ReloadCertificatesOfTrustCenterTest()
+        {
+            #region Arrange
+
+            #endregion
+
+
+            #region Act
+
+            #endregion
+
+
+            #region Assert
+
+            #endregion
+
+        }
+
+        [Fact(DisplayName = "IsTrustCenterInputValidTest")]
+        public void IsTrustCenterInputValidTest()
+        {
+            #region Arrange
+
+            #endregion
+
+
+            #region Act
+
+            #endregion
+
+
+            #region Assert
+
+            #endregion
+
+        }
+    }
+}
